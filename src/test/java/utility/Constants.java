205297ff207e0180179f431dc802bf04ac91a2b5
lalitham22 <lallu2209@gmail.com>
2023-11-20 05:52:34 -0800

    New docs

---
+++ src/test/java/utility/Constants.java
@@ -0,0 +1,15 @@
+package utility;
+
+public class Constants {
+	
+	
+	
+	public static String browserDetails = "sdhajshfohsaoifh";
+	public static String hrmLogin = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+	public static String hrmLogin_prod = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+	
+	
+	
+	
+
+}
